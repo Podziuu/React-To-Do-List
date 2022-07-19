0c41219af93aeac36ed2a78ead73909fb5865475
Bartosz  Podemski <bartekpodemski@gmail.com>
2022-07-19 15:41:56 +0200

    Adding starting page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Starting from './pages/Starting';
 import Navbar from './components/Layout/Navbar';
+import Footer from './components/Layout/Footer';
 
 function App() {
   return (
     <div className="bg-[#0077B6] h-screen">
       <Navbar/>
-      {/* <Starting/> */}
+      <Starting/>
+      <Footer/>
     </div>
   );
 }
--- src/pages/Starting.js
+++ src/pages/Starting.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const Starting = () => {
   return (
-    <div>
-        {/* <h1>To Do App</h1> */}
+    <div className="flex justify-center items-center flex-col h-4/5">
+      <h1 className="text-white font-bold text-6xl">To Do App</h1>
+      <div className="mt-5">
+        <button className="bg-[#03045E] text-white px-8 py-2 rounded-full font-bold text-xl mr-7 cursor-pointer">Sign In</button>
+        <button className="bg-[#90E0EF] text-white px-8 py-2 rounded-full font-bold text-xl cursor-pointer">Sign Up</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Starting
+export default Starting;
